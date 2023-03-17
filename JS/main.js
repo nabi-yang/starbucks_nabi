bb56f6780f4e9a0bb7ad8bfeeceaaae0c0142e5e
nabi-yang <sunae894@naver.com>
2023-03-17 23:29:19 +0900

    뱃지추가
    뱃지추가

--- JS/main.js
+++ JS/main.js
@@ -15,3 +15,25 @@ searchInputEl.addEventListener('blur', function () {
     searchInputEl.setAttribute('placeholder', '');
 });
 
+const badgeEl=document.querySelector('header .badges');
+
+//윈도우화면이 '스크롤'되면 함수를 실행하겠다
+window.addEventListener('scroll', _.throttle(function(){
+    console.log(window.scrollY);
+    if (window.scrollY >500){
+        // badgeEl.style.display ='none';
+        // gsap.to(요소, 지속시간, 옵션);
+        gsap.to(badgeEl, .6, {
+            opacity:0,  // , 까먹지말고 넣어주기
+            display: 'none'
+        });
+    }else{
+        // badgeEl.style.display ='block';
+        gsap.to(badgeEl, .6, {
+            opacity:1,
+            display:'block'
+            
+    });
+}
+}, 300)); //0.3초를 의미한다 
+//_.throttle(함수, 시간)
