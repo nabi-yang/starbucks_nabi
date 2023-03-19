04136f663fe85cbe8fc6b2cb5b31e3424063a6e3
nabi-yang <sunae894@naver.com>
2023-03-19 15:00:02 +0900

    공지사항&프로모션

--- JS/main.js
+++ JS/main.js
@@ -48,3 +48,10 @@ fadeEls.forEach(function(fadeEl, index){
     });
 
 });
+
+// new Swiper(선택자,옵션)
+new Swiper('.notice-line .swiper-container',{
+    direction: 'vertical',
+    autoplay: true,
+    loop: true
+});
